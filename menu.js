e95108be2cdbf1ef414520fcb879d32ac29b6aa3
Amarildo Miguel de Souza <52436750+Amsouza15@users.noreply.github.com>
2023-10-13 22:47:36 -0300

    upload do arquivos index do site do cidadania são Joaquim de bicas  teste implementação

---
+++ menu.js
@@ -0,0 +1,14 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overley = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+menu.addEventListener('click', ()=>{
+    overley.classList.remove('abrir-menu')
+})
